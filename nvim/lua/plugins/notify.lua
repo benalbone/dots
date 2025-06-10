b0e5bc89b7b64d0807bb3ac29ebdb357daecf9bb
Ben Albone <80364113+benalbone@users.noreply.github.com>
2025-06-10 16:36:03 +0100

    nvim-notify configu adjustment
    added the background_colour specification to nvim-notify config to
    prevent constant spam of error notifications when issuing commands

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,1 +1,8 @@
-return { 'rcarriga/nvim-notify' }
+return {
+    'rcarriga/nvim-notify',
+    config = function()
+        require("notify").setup({
+            background_colour = "#000000",
+        })
+    end,
+}
