5bb9f1ba861c3364893dbf0f598ced64c70b72c2
Ben Albone <80364113+benalbone@users.noreply.github.com>
2025-06-14 23:19:02 +0100

    nvim-tree settings
    adjusted config to add in settings, adaptive size so the buffer adjusts
    to the length of file names making sure they always fit

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,13 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup({
+      view = {
+          adaptive_size = true,
+      },
+      filters = {
+          dotfiles = false,
+      }
+    })
   end,
 }
